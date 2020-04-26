5c19a5bbe6f814d00356e9579ef4a526f034bcd1
Bruno Gustavo <brugucolosil95@gmail.com>
2020-04-26 16:51:53 -0300

    feat: add new routes

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -3,10 +3,24 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from '../pages/Home';
+import Signin from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
+import ConfirmEmail from '../pages/ConfirmEmail';
+import ConfirmedEmail from '../pages/ConfirmedEmail';
+import ResendEmail from '../pages/ResendEmail';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route path="/auth/signin" component={Signin} />
+    <Route path="/auth/signup" component={SignUp} />
+    <Route path="/auth/forgot_password" component={ForgotPassword} />
+    <Route path="/auth/reset_password" component={ResetPassword} />
+    <Route path="/auth/confirm_email" component={ConfirmEmail} />
+    <Route path="/auth/confirmed_email" component={ConfirmedEmail} />
+    <Route path="/auth/resend_email" component={ResendEmail} />
   </Switch>
 );
 
