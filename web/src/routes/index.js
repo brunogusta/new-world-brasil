a38244bbc883e6dcb6500e96bfe7adca25626e28
Bruno Gustavo <brugucolosil95@gmail.com>
2020-06-09 18:57:46 -0300

    feat: add new pages in routes and private-route

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Route, Switch } from 'react-router-dom';
 
+import PrivateRoute from './privateRoute';
+
 import Home from '../pages/Home';
 import Signin from '../pages/auth/SignIn';
 import SignUp from '../pages/auth/SignUp';
@@ -14,21 +16,31 @@ import CreateCompany from '../pages/CreateCompany';
 import ListCompanies from '../pages/ListCompanies';
 import CompanyPage from '../pages/CompanyPage';
 import CompanyUpdate from '../pages/CompanyUpdate';
+import NewPost from '../pages/NewPost';
+import UserProfile from '../pages/UserProfile';
+import NotFound404 from '~/pages/NotFound404.js';
+import GuidesList from '~/pages/GuidesList';
+import GuidePage from '~/pages/GuidePage';
 
 const Routes = () => (
   <Switch>
+    <PrivateRoute path="/new-post" component={NewPost} />
     <Route exact path="/" component={Home} />
     <Route path="/auth/signin" component={Signin} />
     <Route path="/auth/signup" component={SignUp} />
     <Route path="/auth/forgot-password" component={ForgotPassword} />
-    <Route path="/auth/reset-password" component={ResetPassword} />
+    <Route path="/auth/reset-password/:token" component={ResetPassword} />
     <Route path="/auth/confirm-email" component={ConfirmEmail} />
-    <Route path="/auth/confirmed-email" component={ConfirmedEmail} />
+    <Route path="/auth/confirmed-email/:token" component={ConfirmedEmail} />
     <Route path="/auth/resend-email" component={ResendEmail} />
     <Route path="/registry/company/create-company" component={CreateCompany} />
     <Route path="/registry/company/update" component={CompanyUpdate} />
-    <Route exact path="/companies" component={ListCompanies} />
-    <Route path="/companies/find-one" component={CompanyPage} />
+    <Route exact path="/companies/all/:page" component={ListCompanies} />
+    <Route path="/companies/find-one/:id" component={CompanyPage} />
+    <Route path="/user-profile" component={UserProfile} />
+    <Route path="/list-guides" component={GuidesList} />
+    <Route path="/guide/:id" component={GuidePage} />
+    <Route component={NotFound404} />
   </Switch>
 );
 
