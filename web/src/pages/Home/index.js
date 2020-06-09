d40035cde0cfd315d60d369a6208e3da15056512
Bruno Gustavo <brugucolosil95@gmail.com>
2020-06-09 18:50:46 -0300

    feat: add new page home

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -1,15 +1,400 @@
-import React from 'react';
+/* eslint-disable no-nested-ternary */
+/* eslint-disable no-unneeded-ternary */
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable no-underscore-dangle */
+import React, { useEffect, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import { Carousel } from 'react-responsive-carousel';
+import moment from 'moment';
 
-import { Container, Presentation, Content } from './styles';
-import banner from '~/assets/images/banner_home.jpg';
+import {
+  Container,
+  ContentGridWrapper,
+  SiteInformationsWrapper,
+  LivesContentWrapper,
+  SideContentWrapper,
+  NoCompanyText,
+  PremiumCompanies,
+  SocialNetwordWrapper,
+  DiscordButton,
+  SocialButtonsWrapper,
+  FeedContentWrapper,
+  CardWrapper,
+  CardHeader,
+  CompanyName,
+  CardFooter,
+  InfoWrapper,
+  CardLiveWrapper,
+  LivesHeaderText,
+  FeedBar,
+  PostWrapper,
+  PostHeader,
+  TitlePostWrapper,
+  PostContentWrapper,
+  PostDate,
+  Content,
+  Footer,
+} from './styles';
 
-const Home = () => (
-  <Container>
-    <Presentation className="presentation">
-      <img src={banner} alt="banner" />
-    </Presentation>
-    <Content />
-  </Container>
-);
+import bar from '~/assets/images/bar.png';
+import cardBackground from '~/assets/images/company-art.png';
+import arrowLeft from '~/assets/images/arrow-left.png';
+import arrowRight from '~/assets/images/arrow-right.png';
+import instagram from '~/assets/images/instagram.png';
+import youtube from '~/assets/images/youtube.png';
+import twitter from '~/assets/images/twitter.png';
+import facebook from '~/assets/images/facebook.png';
+
+import api from '~/services/api';
+import Pagination from '~/components/Pagination';
+import LoadingAnimation from '~/utils/animation/LoadingAnimation';
+
+const Home = () => {
+  const [totalMembers, setTotalMembers] = useState(null);
+  const [totalCompanies, setTotalCompanies] = useState(null);
+  const [totalPages, setTotalPages] = useState(1);
+  const [companies, setCompanies] = useState(null);
+  const [posts, setPosts] = useState(null);
+  const [liveCards] = useState([1, 2, 3, 4]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      await api
+        .get('/companies/total')
+        .then(({ data }) => {
+          setTotalMembers(data.totalMembers);
+          setTotalCompanies(data.totalCompanies);
+        })
+        .catch((e) => console.log(e));
+    }
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      await api
+        .get('/companies/supporters')
+        .then(({ data }) => {
+          console.log(data);
+          if (data) {
+            setCompanies(data);
+          }
+          setLoading(false);
+        })
+        .catch((e) => {
+          console.log(e);
+          setLoading(false);
+          if (e.response && e.response.data.error) {
+            toast.error(`${e.response.data.error}`, {
+              position: toast.POSITION.TOP_CENTER,
+              draggable: false,
+              autoClose: 8000,
+              className: 'custom-toast',
+            });
+          } else {
+            toast.error('Erro inesperado ao carregar a lista de companhias', {
+              position: toast.POSITION.TOP_CENTER,
+              draggable: false,
+              autoClose: 8000,
+              className: 'custom-toast',
+            });
+          }
+        });
+    }
+
+    fetchData();
+    setLoading(true);
+  }, []);
+
+  const params = new URL(document.location).searchParams;
+  let page = params.get('page');
+  if (!page) page = 1;
+  useEffect(() => {
+    async function fetchData() {
+      await api
+        .get(`/post/load-posts/?page=${page}`)
+        .then(({ data }) => {
+          setPosts(data.posts);
+          setTotalPages(data.totalPages);
+        })
+        .catch((e) => {
+          if (e.response.data && e.response.data.error) {
+            toast.error(`${e.response.data.error}`, {
+              position: toast.POSITION.TOP_CENTER,
+              draggable: false,
+              autoClose: 8000,
+              className: 'custom-toast',
+            });
+          }
+        });
+    }
+
+    fetchData();
+  }, []);
+
+  const createCarouselItemCard = (index) => {
+    return (
+      <CardLiveWrapper key={index}>
+        <a
+          href="https://www.youtube.com/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img src={cardBackground} alt="card" />
+        </a>
+      </CardLiveWrapper>
+    );
+  };
+
+  const baseChildren = <div>{liveCards.map(createCarouselItemCard)}</div>;
+
+  // eslint-disable-next-line no-restricted-globals
+  const screenWidth = screen.width;
+  const getConfigurableProps = () => ({
+    showArrows: liveCards.length > 3,
+    interval: 2000,
+    infiniteLoop: liveCards.length > 3,
+    autoPlay: liveCards.length > 3,
+    showIndicators: false,
+    stopOnHover: true,
+    showStatus: false,
+    showThumbs: false,
+    centerMode: screenWidth < '750' ? false : true,
+    swipeable: false,
+    centerSlidePercentage: liveCards.length === 3 ? 30 : 33,
+  });
+
+  return (
+    <Container>
+      <ToastContainer />
+      <ContentGridWrapper>
+        <SiteInformationsWrapper>
+          <div className="wrapper">
+            <span className="logo">
+              <i className="fab fa-acquisitions-incorporated" />
+              CENTRAL NWBR
+            </span>
+            <div>
+              <span>
+                <i className="fas fa-users" />
+                {totalMembers} {totalMembers > 1 ? 'MEMBROS' : 'MEMBRO'}
+              </span>
+              <span> | </span>
+              <span>
+                <i className="fas fa-home" />
+                {totalCompanies}
+                {totalCompanies > 1 ? ' COMPANHIAS' : ' COMPANHIA'}
+              </span>
+            </div>
+          </div>
+        </SiteInformationsWrapper>
+        <LivesContentWrapper>
+          <LivesHeaderText>
+            <h1>LIVES DE NEW WORLD</h1>
+          </LivesHeaderText>
+          <Carousel
+            {...getConfigurableProps()}
+            renderArrowPrev={(onClickHandler, hasPrev, label) =>
+              hasPrev && (
+                <button
+                  type="button"
+                  onClick={onClickHandler}
+                  title={label}
+                  style={{
+                    position: 'absolute',
+                    backgroundColor: 'transparent',
+                    border: 'none',
+                    left: -30,
+                    top: 0,
+                    bottom: 0,
+                    zIndex: 10,
+                  }}
+                >
+                  <img
+                    src={arrowLeft}
+                    alt="arrow-left"
+                    style={{
+                      maxWidth: '40px',
+                      height: 'auto',
+                      position: 'relative',
+                    }}
+                  />
+                </button>
+              )}
+            renderArrowNext={(onClickHandler, hasNext, label) =>
+              hasNext && (
+                <button
+                  type="button"
+                  onClick={onClickHandler}
+                  title={label}
+                  style={{
+                    position: 'absolute',
+                    backgroundColor: 'transparent',
+                    border: 'none',
+                    bottom: 0,
+                    top: 0,
+                    right: -30,
+                    zIndex: 10,
+                  }}
+                >
+                  <img
+                    src={arrowRight}
+                    alt="arrow-left"
+                    style={{
+                      maxWidth: '40px',
+                      height: 'auto',
+                      position: 'relative',
+                    }}
+                  />
+                </button>
+              )}
+          >
+            {baseChildren.props.children}
+          </Carousel>
+        </LivesContentWrapper>
+        <SideContentWrapper>
+          <PremiumCompanies>
+            <span>
+              <h1>
+                COMPANHIAS <i className="fas fa-star" />
+              </h1>
+            </span>
+            <div className="companies-list">
+              {companies ? (
+                companies.map(({ _companyId }) => {
+                  return (
+                    <CardWrapper
+                      href={`http://localhost:3000/companies/find-one/${_companyId._id}`}
+                      target="_blank"
+                    >
+                      <img
+                        className="background-company"
+                        src={cardBackground}
+                        alt="background-company"
+                      />
+                      <CardHeader>
+                        <p>COMPANHIA</p>
+                        <i className="fas fa-star" />
+                      </CardHeader>
+                      <CompanyName>
+                        <h1>{_companyId.title}</h1>
+                        <img src={bar} alt="bar" />
+                      </CompanyName>
+                      <CardFooter>
+                        <InfoWrapper>
+                          <p className="info-name">FACÇÃO</p>
+                          <p>{_companyId.faction}</p>
+                        </InfoWrapper>
+                        <InfoWrapper>
+                          <p className="info-name">MEMBROS</p>
+                          <p>{_companyId.size}</p>
+                        </InfoWrapper>
+                        <InfoWrapper>
+                          <p className="info-name">RECRUTANDO</p>
+                          <p>{_companyId.recruting ? 'SIM' : 'NÃO'}</p>
+                        </InfoWrapper>
+                      </CardFooter>
+                    </CardWrapper>
+                  );
+                })
+              ) : loading ? (
+                <LoadingAnimation />
+              ) : (
+                <NoCompanyText>
+                  <h1>Não há companhias em destaque.</h1>{' '}
+                </NoCompanyText>
+              )}
+            </div>
+          </PremiumCompanies>
+          <div className="bar-division">
+            <img src={bar} alt="bar" />
+          </div>
+          <SocialNetwordWrapper>
+            <p className="discord-button-label">COMUNIDADE NEW WORLD BRASIL</p>
+            <DiscordButton>
+              <a
+                href="https://discord.gg/hh6Jbzk"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <div className="btn-content">
+                  <i className="fab fa-discord" />
+                  <div>
+                    <p>Link direto para o</p>
+                    <h1>DISCORD</h1>
+                  </div>
+                </div>
+              </a>
+            </DiscordButton>
+            <SocialButtonsWrapper>
+              <a href="#">
+                <img src={facebook} alt="facebook-icon" />
+              </a>
+              <a href="#">
+                <img src={instagram} alt="instagram-icon" />
+              </a>
+              <a href="#">
+                <img src={twitter} alt="twitter-icon" />
+              </a>
+              <a href="#">
+                <img src={youtube} alt="youtube-icon" />
+              </a>
+            </SocialButtonsWrapper>
+          </SocialNetwordWrapper>
+          <div className="bar-division">
+            <img src={bar} alt="bar" />
+          </div>
+        </SideContentWrapper>
+        <FeedBar>
+          <div>
+            <h1>FEED NWBR</h1>
+          </div>
+        </FeedBar>
+        <FeedContentWrapper>
+          {posts ? (
+            posts.map((post) => {
+              return (
+                <PostWrapper>
+                  <PostHeader>
+                    <TitlePostWrapper>
+                      <h1>{post.title}</h1>
+                    </TitlePostWrapper>
+                    <img src={post.imageUrl} alt={post.imageName} />
+                  </PostHeader>
+                  <PostContentWrapper>
+                    <PostDate>
+                      <h1>
+                        {moment(post.createdAt)
+                          .locale('pt-br')
+                          .format('DD/MM/YYYY')}
+                      </h1>
+                    </PostDate>
+                    <Content
+                      dangerouslySetInnerHTML={{ __html: post.description }}
+                    />
+                    <div className="end-bar">
+                      <img src={bar} alt="end-bar" />
+                    </div>
+                  </PostContentWrapper>
+                </PostWrapper>
+              );
+            })
+          ) : (
+            <LoadingAnimation />
+          )}
+        </FeedContentWrapper>
+        <Footer>
+          <Pagination
+            total={totalPages}
+            activePage={Number(page)}
+            pageLink="http://localhost:3000/?page=%page%"
+          />
+        </Footer>
+      </ContentGridWrapper>
+    </Container>
+  );
+};
 
 export default Home;
--- web/src/pages/Home/styles.js
+++ web/src/pages/Home/styles.js
@@ -1,30 +1,562 @@
 import styled from 'styled-components';
+import media from 'styled-media-query';
+
+import banner from '~/assets/images/banner_home.jpg';
+import background from '~/assets/images/background.jpg';
+import baseAnimation from '~/utils/animation/base-animation';
 
 export const Container = styled.div`
-  grid-template-columns: 1fr;
-  grid-template-rows: minmax(80vh, 1fr) minmax(80vh, 1fr) minmax(30vh, 1fr);
-  grid-template-areas:
-    'presentation'
-    'content'
-    'footer';
+  background-image: url(${banner});
+  background-position: initial;
+  background-repeat: no-repeat;
+  background-size: contain;
 
-  min-height: 80vh;
+
+  border: solid 1px transparent;
+
+
+  ${media.lessThan('750px')`
+    background-image: url(${background});
+  `}
+
+  ${media.between('750px', '1366px')`
+    background-image: url(${background});
+  `}
+
+
   background-color: ${({ theme }) => theme.background};
 `;
 
-export const Presentation = styled.div`
-  min-height: 30vh;
+export const ContentGridWrapper = styled(baseAnimation)`
+  max-width: 1920px;
 
+
+  ${media.between('1050px', '1440px')`
+    width: 90%;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: auto;
+  `}
+
+  ${media.between('1441px', '1920px')`
+    width: 70%;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: auto;
+  `}
+
+
+
+  ${media.lessThan('750px')`
+    margin-top: 5em;
+  `}
+
+  ${media.between('751px', '1366px')`
+    margin-top: 5em;
+  `}
+
+  ${media.between('1367px', '1440px')`
+    margin-top: 29em;
+  `}
+
+  ${media.between('1441px', '1920px')`
+    margin-top: 39em;
+  `}
+
+
+  display: grid;
+  grid-template-areas:
+    'site-informations site-informations'
+    'lives-content side-content'
+    'feed-bar side-content'
+    'feed-content side-content'
+    'footer side-content';
+
+  grid-template-columns: minmax(750px, 4fr) minmax(330px, 1fr);
+  grid-template-rows: 50px minmax(100px, 200px)  50px 1fr 50px;
+  grid-row-gap: 20px;
+  grid-column-gap: 20px;
+
+  ${media.lessThan('1024px')`
+    grid-template-areas:
+      'site-informations'
+      'lives-content'
+      'feed-bar'
+      'feed-content'
+      'footer';
+
+    grid-template-columns: minmax(100px, auto);
+    grid-template-rows: 50px  minmax(100px, 200px)  50px 2fr 50px;
+    grid-row-gap: 20px;
+  `}
+`;
+
+export const SiteInformationsWrapper = styled.div`
+  grid-area: site-informations;
+
+
+.wrapper {
+  border-radius: 10px;
+  background-color: rgba(0, 0, 0, 0.3);
+
+  display: flex;
+
+  padding: 10px;
+  justify-content: space-between;
+  align-items: center;
+
+
+  ${media.lessThan('750px')`
+    .logo {
+      display: none;
+    };
+
+    justify-content: center;
+  `}
+
+  span {
+    i {
+      margin-right: 5px;
+    }
+
+    font-family: 'av-bold';
+    font-size: 1.2rem;
+    color: #fff;
+
+    ${media.lessThan('320px')`
+      font-size: 0.83rem;
+    `}
+
+    ${media.between('360px', '410px')`
+      font-size: 0.96rem;
+    `}
+
+    ${media.between('411px', '750px')`
+      font-size: 1.13rem;
+    `}
+  }
+}
+`;
+
+export const LivesContentWrapper = styled.div`
+  grid-area: lives-content;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .carousel .slider-wrapper .slide {
+    background-color: transparent;
+  }
+
+  .carousel {
+    overflow: visible;
+  }
+
+  .carousel-root {
+    max-width: 100%;
+    padding: 0 20px;
+  }
+
+  .slider-wrapper {
+  }
+
+  ${media.greaterThan('1050px')`
+    min-width: 750px;
+  `}
+
+  ${media.lessThan('750px')`
+    h1 {
+     font-size: 1.6rem;
+    }
+
+    .carousel-root {
+      padding: 0 30px;
+    }
+  `}
+`;
+
+export const LivesHeaderText = styled.div`
+  margin-bottom: 10px;
+  h1 {
+    font-family: 'av-bold';
+    color: #fff;
+    text-shadow: 3px 5px 2px rgba(0, 0, 0, 0.3);
+  }
+`;
+
+export const FeedContentWrapper = styled.div`
+  grid-area: feed-content;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const FeedBar = styled.div`
+  grid-area: feed-bar;
+
+  div {
+    border-radius: 10px;
+    padding: 10px;
+    background-color: rgba(0, 0, 0, 0.4);
+
+    h1 {
+      font-size: 1.2rem;
+      font-family: 'av-bold';
+      color: #fff;
+    }
+  }
+`;
+
+export const PostWrapper = styled.div`
+  position: relative;
+  min-height: 500px;
+  border-radius: 30px;
+  overflow: hidden;
+  background-color: #6a6e70;
+
+  margin-bottom: 30px;
+`;
+
+export const PostHeader = styled.div`
   img {
     height: auto;
     max-width: 100%;
   }
 `;
 
+export const TitlePostWrapper = styled.div`
+  position: absolute;
+  width: 100%;
+  color: #fff;
+  font-family: 'av-bold';
+  text-transform: uppercase;
+  text-align: center;
+  padding: 10px 5px;
+
+  background-color: rgba(0, 0, 0, 0.6);
+
+  top: 0;
+
+  ${media.lessThan('750px')`
+    font-size: 0.8rem;
+  `}
+`;
+
+export const PostContentWrapper = styled.div`
+  padding: 10px;
+
+  .end-bar {
+    display: flex;
+    justify-content: center;
+
+    margin: 20px auto;
+    height: 10px;
+    max-width: 90%;
+  }
+`;
+
+export const PostDate = styled.div`
+  h1 {
+    font-size: 0.9rem;
+    font-family: 'av-light';
+    color: #fff;
+  }
+`;
+
 export const Content = styled.div`
-  min-height: 50vh;
-  background-position: center;
-  background-color: ${({ theme }) => theme.background};
-  background-repeat: repeat repeat;
-  background-size: cover;
+  margin-top: 10px;
+
+  p {
+    line-height: 30px;
+    font-family: 'av-light';
+    text-align: justify;
+    color: #fff;
+    font-size: 1.3rem;
+  }
+`;
+
+export const CardLiveWrapper = styled.div`
+  position: relative;
+  margin: 0 10px;
+  height: 140px;
+
+  img {
+    border-radius: 10px;
+    display: block;
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const SideContentWrapper = styled.div`
+  grid-area: side-content;
+
+  .bar-division {
+    img {
+      margin: 30px 0px 20px 0px;
+      height: 10px;
+      max-width: 100%;
+    }
+  }
+
+  ${media.lessThan('1024px')`
+    display: none;
+  `}
+`;
+
+export const PremiumCompanies = styled.div`
+  span {
+    h1 {
+      font-family: 'av-bold';
+      text-align: center;
+      color: #fff;
+      text-shadow: 3px 5px 2px rgba(0, 0, 0, 0.3);
+
+      i {
+        font-size: 2rem;
+        color: #ffc700;
+      }
+    }
+  }
+
+  .companies-list {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    max-height: 750px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    padding: 0px 10px 10px 10px;
+
+    ::-webkit-scrollbar {
+      width: 10px;
+    }
+
+    ::-webkit-scrollbar-track {
+      width: 10px;
+      background-color: #243035;
+      border-radius: 10px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background-color: #6a6e70;
+      -webkit-border-radius: 10px;
+      border-radius: 10px;
+    }
+  }
+`;
+
+export const NoCompanyText = styled.div`
+  padding-top: 20px;
+  h1 {
+    text-align: center;
+    font-family: 'av-light';
+    font-size: 1.5rem;
+    color: #fff;
+  }
+`;
+
+export const SocialNetwordWrapper = styled.div`
+  .discord-button-label {
+    font-family: 'av-light';
+    font-size: 1.2rem;
+    color: #fff;
+    text-align: center;
+    margin-bottom: 10px;
+  }
+`;
+
+export const SocialButtonsWrapper = styled.div`
+  padding-top: 30px;
+  display: flex;
+  justify-content: space-between;
+
+  img {
+    height: auto;
+    max-width: 54px;
+  }
+`;
+
+export const DiscordButton = styled.div`
+  position: relative;
+  width: 100%;
+
+  .btn-content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0 10px;
+
+    p {
+      margin-left: 10px;
+      font-family: 'av-light';
+      font-size: 1.8rem;
+    }
+
+    h1 {
+      margin-left: 10px;
+      line-height: 40px;
+      font-family: 'av-bold';
+      letter-spacing: 2px;
+      font-size: 3rem;
+    }
+  }
+
+  i {
+    font-size: 5rem;
+  }
+
+  a {
+    padding: 10px 0px;
+    text-decoration: none;
+    display: block;
+    text-align: center;
+    position: relative;
+    color: rgba(255, 255, 255, 1);
+    background-color: #7289da;
+    -webkit-border-radius: 8px;
+    -moz-border-radius: 8px;
+    border-radius: 8px;
+    -webkit-box-shadow: 0px 9px 0px #334dad, 0px 9px 25px rgba(0, 0, 0, 0.7);
+    -moz-box-shadow: 0px 9px 0px #334dad, 0px 9px 25px rgba(0, 0, 0, 0.7);
+    box-shadow: 0px 9px 0px #334dad 0px 9px 25px rgba(0, 0, 0, 0.7);
+
+    -webkit-transition: all 0.1s ease;
+    -moz-transition: all 0.1s ease;
+    -ms-transition: all 0.1s ease;
+    -o-transition: all 0.1s ease;
+    transition: all 0.1s ease;
+  }
+
+  a:hover {
+    background-color: #5069c5;
+    -webkit-box-shadow: 0px 3px 0px #334dad, 0px 3px 6px rgba(0, 0, 0, 0.9);
+    -moz-box-shadow: 0px 3px 0px #334dad, 0px 3px 6px rgba(0, 0, 0, 0.9);
+    box-shadow: 0px 3px 0px #334dad, 0px 3px 6px rgba(0, 0, 0, 0.9);
+    position: relative;
+    top: 6px;
+  }
+`;
+
+export const CardWrapper = styled.a`
+  position: relative;
+  display: flex;
+  text-decoration: none;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 10px;
+
+  margin: 10px 0;
+
+  border-radius: 10px;
+
+  width: 100%;
+  transition: all 200ms ease-in-out;
+
+  &:hover {
+    transform: scale(1.04);
+  }
+
+  .background-company {
+    top: 0;
+    left: 0;
+    position: absolute;
+    z-index: 1;
+    max-height: 100%;
+    width: 100%;
+  }
+`;
+
+export const CardHeader = styled.div`
+  position: relative;
+  display: flex;
+
+  align-items: center;
+  justify-content: center;
+  z-index: 2;
+
+  i {
+    font-size: 1.2rem;
+    position: absolute;
+    color: #ffc700;
+    right: 0;
+  }
+
+  p {
+    font-size: 0.8rem;
+    font-family: 'av-light';
+    font-weight: 900;
+    color: #fff;
+    text-transform: uppercase;
+  }
+`;
+
+export const CompanyName = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  z-index: 2;
+
+  img {
+    height: 10px;
+    max-width: 90%;
+  }
+
+  h1 {
+    text-transform: capitalize;
+    font-family: 'av-light';
+    font-weight: 900;
+    color: #fff;
+  }
+`;
+
+export const CardFooter = styled.div`
+  z-index: 2;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const InfoWrapper = styled.div`
+  z-index: 2;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 15px;
+
+  p {
+    font-size: 1rem;
+    font-family: 'av-light';
+    font-weight: 900;
+    color: #fff;
+    text-transform: uppercase;
+  }
+
+  .info-name {
+    font-size: 0.6rem;
+    font-family: 'av-light';
+    font-weight: 900;
+    color: #fff;
+  }
+`;
+
+export const Footer = styled.div`
+  grid-area: footer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 0;
+
+  margin-bottom: 10px;
+`;
+
+export const LoadingWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 100px;
 `;
