1e6f23b6dddfaff4cfba37940e06c420bd9d42a7
Bruno Gustavo <brugucolosil95@gmail.com>
2020-06-09 18:54:11 -0300

    feat: add pagination stories

--- web/src/components/Pagination/pagination.stories.js
+++ web/src/components/Pagination/pagination.stories.js
@@ -5,4 +5,10 @@ import Pagination from './index';
 
 export default { title: 'Pagination' };
 
-export const withoutProps = () => <Pagination />;
+export const withoutProps = () => (
+  <Pagination
+    total={10}
+    activePage={5}
+    pageLink="http://localhost:3000/companies/?page=%page%"
+  />
+);
