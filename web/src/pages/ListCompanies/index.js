e49792de35a24a9fcd7d629b8f2cb19e88751aa6
Bruno Gustavo <brugucolosil95@gmail.com>
2020-06-09 18:51:57 -0300

    feat: add new page list-companies

--- web/src/pages/ListCompanies/index.js
+++ web/src/pages/ListCompanies/index.js
@@ -1,9 +1,120 @@
-import React from 'react';
+/* eslint-disable no-underscore-dangle */
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import Loading from '~/utils/animation/LoadingAnimation';
 
-import { Container } from './styles';
+import {
+  Container,
+  SloganWrapper,
+  ListCompaniesWrapper,
+  CompaniesGrid,
+  CardWrapper,
+  CardHeader,
+  CompanyName,
+  CardFooter,
+  InfoWrapper,
+  Footer,
+  LoadingWrapper,
+} from './styles';
+
+import bar from '~/assets/images/bar.png';
+import api from '~/services/api';
+import Pagination from '~/components/Pagination';
+import cardBackground from '~/assets/images/company-art.png';
 
 const ListCompanies = () => {
-  return <Container />;
+  const [companies, setCompanies] = useState(false);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const { page } = useParams();
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const { data } = await api.get(`/companies/all/${page}`);
+
+        console.log(data);
+        setCompanies(data.companies);
+        setTotalPages(data.totalPages);
+      } catch (e) {
+        if (e.response.data && e.response.data.error) {
+          toast.error(`${e.response.data.error}`, {
+            position: toast.POSITION.TOP_CENTER,
+            draggable: false,
+            autoClose: 8000,
+            className: 'custom-toast',
+          });
+        }
+      }
+    }
+    fetchData();
+  }, [page]);
+
+  return (
+    <Container>
+      <ToastContainer />
+      <SloganWrapper>
+        <p className="top-text">CENTRAL DAS</p>
+        <p className="middle-text">COMPANHIAS</p>
+        <p className="bottom-text">NEW WORLD BRASIL</p>
+      </SloganWrapper>
+      {companies ? (
+        <>
+          <ListCompaniesWrapper>
+            <CompaniesGrid>
+              {companies.map((company) => (
+                <CardWrapper
+                  href={`http://localhost:3000/companies/find-one/${company._id}`}
+                  target="_blank"
+                >
+                  <img
+                    className="background-image"
+                    src={cardBackground}
+                    alt="background"
+                  />
+                  <CardHeader>
+                    {company._creatorId.isSupporter ? (
+                      <i className="fas fa-star" />
+                    ) : null}
+                  </CardHeader>
+                  <CompanyName>
+                    <p className="company-field">COMPANHIA</p>
+                    <p>{company.title}</p>
+                    <img src={bar} alt="bar" />
+                  </CompanyName>
+                  <CardFooter>
+                    <InfoWrapper>
+                      <p className="info-name">FACÇÃO</p>
+                      <p>{company.faction}</p>
+                    </InfoWrapper>
+                    <InfoWrapper>
+                      <p className="info-name">MEMBROS</p>
+                      <p>{company.size}</p>
+                    </InfoWrapper>
+                    <InfoWrapper>
+                      <p className="info-name">RECRUTANDO</p>
+                      <p>{company.recruting ? 'SIM' : 'NÃO'}</p>
+                    </InfoWrapper>
+                  </CardFooter>
+                </CardWrapper>
+              ))}
+            </CompaniesGrid>
+          </ListCompaniesWrapper>
+          <Footer>
+            <Pagination
+              total={totalPages}
+              activePage={Number(page)}
+              pageLink="http://localhost:3000/companies/?page=%page%"
+            />
+          </Footer>
+        </>
+      ) : (
+        <LoadingWrapper>
+          <Loading />
+        </LoadingWrapper>
+      )}
+    </Container>
+  );
 };
 
 export default ListCompanies;
--- web/src/pages/ListCompanies/styles.js
+++ web/src/pages/ListCompanies/styles.js
@@ -1,3 +1,206 @@
 import styled from 'styled-components';
+import media from 'styled-media-query';
+import background from '~/assets/images/background.jpg';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url(${background});
+`;
+
+export const SloganWrapper = styled.div`
+  min-height: 50vh;
+  width: 100%;
+  margin-top: 40px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: 'av-light';
+  letter-spacing: 2px;
+  color: #fff;
+  text-shadow: 4px 3px 0 rgba(0, 0, 0, 0.3);
+
+  .top-text {
+    font-size: 2rem;
+  }
+
+  .middle-text {
+    font-size: 6rem;
+    font-family: 'av-bold';
+  }
+
+  .bottom-text {
+    font-size: 2rem;
+  }
+
+  ${media.lessThan('750px')` /* menor que 450px */
+  .top-text {
+    font-family: 'av-bold';
+    font-size: 1.5rem;
+  }
+
+  .middle-text {
+    font-size: 3rem;
+    font-family: 'av-bold';
+  }
+
+  .bottom-text {
+    font-family: 'av-bold';
+    font-size: 1.5rem;
+  }
+  `}
+
+  ${media.lessThan('330px')` /* menor que 450px */
+  .top-text {
+    font-family: 'av-bold';
+    font-size: 1.2rem;
+  }
+
+  .middle-text {
+    font-size: 2.6rem;
+    font-family: 'av-bold';
+  }
+
+  .bottom-text {
+    font-family: 'av-bold';
+    font-size: 1.2rem;
+  }
+  `}
+`;
+
+export const ListCompaniesWrapper = styled.div`
+  width: 100%;
+  padding: 20px;
+
+  ${media.lessThan('400px')` /* menor que 450px */
+    padding: 0;
+  `}
+`;
+
+export const CompaniesGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  grid-column-gap: 20px;
+  grid-row-gap: 20px;
+`;
+
+export const CardWrapper = styled.a`
+  position: relative;
+  display: flex;
+  text-decoration: none;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 10px;
+
+  border-radius: 10px;
+
+  height: 160px;
+  width: 100%;
+  transition: all 200ms ease-in-out;
+
+  ${media.greaterThan('600px')` /* menor que 450px */
+    &:hover {
+      transform: scale(1.08);
+    }
+  `}
+
+  .background-image {
+    top: 0;
+    left: 0;
+    position: absolute;
+    z-index: 1;
+    max-height: 100%;
+    width: 100%;
+  }
+`;
+
+export const CardHeader = styled.div`
+  position: relative;
+  display: flex;
+
+  justify-content: center;
+  z-index: 2;
+
+  i {
+    font-size: 1.2rem;
+    position: absolute;
+    color: #ffc700;
+    right: 0;
+  }
+`;
+
+export const CompanyName = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  z-index: 2;
+
+  img {
+    height: 10px;
+    max-width: 90%;
+  }
+
+  p {
+    font-size: 1.6rem;
+    font-family: 'av-light';
+    font-weight: 900;
+    color: #fff;
+    text-transform: uppercase;
+  }
+
+  .company-field {
+    font-size: 0.9rem;
+    font-family: 'av-light';
+    font-weight: 900;
+    color: #fff;
+  }
+`;
+
+export const CardFooter = styled.div`
+  z-index: 2;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const InfoWrapper = styled.div`
+  z-index: 2;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  p {
+    font-size: 1.3rem;
+    font-family: 'av-light';
+    font-weight: 900;
+    color: #fff;
+    text-transform: uppercase;
+  }
+
+  .info-name {
+    font-size: 0.8rem;
+    font-family: 'av-light';
+    font-weight: 900;
+    color: #fff;
+  }
+`;
+
+export const Footer = styled.div`
+  z-index: 2;
+  display: flex;
+  justify-content: center;
+  padding: 10px 0;
+`;
+
+export const LoadingWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 100px;
+`;
